4684f9fd7f85d937a2c548a280e65045e892979a
Danial <sdnoaein@gmail.com>
2023-09-25 19:56:42 +0330

    feat: add router

---
+++ src/routes/router.tsx
@@ -0,0 +1,14 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "../pages/home";
+const Router = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default Router;
